e291a88ca795330dd637748e81e627fd26e3915e
Gyula Németh <gyula@chamaileon.io>
2023-08-10 07:12:12 +0200

    Day 10 - #100DaysOfCode

---
+++ standard-lib/02-array-list.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const eql = std.mem.eql;
+const ArrayList = std.ArrayList;
+const test_allocator = std.testing.allocator;
+
+test "arraylist" {
+    var list = ArrayList(u8).init(test_allocator);
+    defer list.deinit();
+
+    try list.append('H');
+    try list.append('e');
+    try list.append('l');
+    try list.append('l');
+    try list.append('o');
+    try list.appendSlice(" World!");
+
+    try expect(eql(u8, list.items, "Hello World!"));
+}
---
+++ standard-lib/03-filesystem.zig
@@ -0,0 +1,51 @@
+const std = @import("std");
+const expect = std.testing.expect;
+const eql = std.mem.eql;
+
+test "createFile, write, seekTo, read" {
+    const file = try std.fs.cwd().createFile("junk_file.txt", .{ .read = true });
+    defer file.close();
+
+    const bytes_written = try file.writeAll("Hello File!");
+    _ = bytes_written;
+
+    var buffer: [100]u8 = undefined;
+    try file.seekTo(0);
+    const bytes_read = try file.readAll(&buffer);
+
+    try expect(eql(u8, buffer[0..bytes_read], "Hello File!"));
+}
+
+test "file stat" {
+    const file = try std.fs.cwd().createFile("junk_file2.txt", .{ .read = true });
+    defer file.close();
+
+    const stat = try file.stat();
+
+    try expect(stat.size == 0);
+    try expect(stat.kind == .File);
+    try expect(stat.ctime <= std.time.nanoTimestamp());
+    try expect(stat.mtime <= std.time.nanoTimestamp());
+    try expect(stat.atime <= std.time.nanoTimestamp());
+}
+
+test "make dir" {
+    try std.fs.cwd().makeDir("test-tmp");
+    const iter_dir = try std.fs.cwd().openIterableDir("test-tmp", .{});
+
+    defer {
+        std.fs.cwd().deleteTree("test-tmp") catch unreachable;
+    }
+
+    _ = try iter_dir.dir.createFile("x", .{});
+    _ = try iter_dir.dir.createFile("y", .{});
+    _ = try iter_dir.dir.createFile("z", .{});
+
+    var file_count: usize = 0;
+    var iter = iter_dir.iterate();
+    while (try iter.next()) |entry| {
+        if (entry.kind == .File) file_count += 1;
+    }
+
+    try expect(file_count == 3);
+}
