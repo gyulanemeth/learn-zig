276acabfdf5ed7bee66111938189d03fb13b2124
Gyula Németh <gyula@chamaileon.io>
2023-08-18 12:37:17 +0200

    Day 18 - #100DaysOfCode - Game of life kinda works

--- game-of-life/main.zig
+++ game-of-life/main.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 const state_path = "./state.txt";
 
 var state: [10][10]u8 = undefined;
+var next_state: [10][10]u8 = undefined;
 
 fn loadState() !void {
     var state_file = try std.fs.cwd().openFile(state_path, .{});
@@ -19,13 +20,14 @@ fn loadState() !void {
 
         for (line) |char, idx| {
             state[line_num][idx] = char - 48; // 0 in ascii is 48
+            next_state[line_num][idx] = 0;
         }
     }
 }
 
 fn saveState() !void {}
 
-fn cellValue(r_idx: usize, c_idx: usize) u8 {
+fn cellValue(r_idx: i32, c_idx: i32) u8 {
     var real_r_idx = r_idx;
     var real_c_idx = c_idx;
 
@@ -37,13 +39,13 @@ fn cellValue(r_idx: usize, c_idx: usize) u8 {
         real_c_idx += 10;
     }
 
-    real_r_idx = real_r_idx % 10;
-    real_c_idx = real_c_idx % 10;
+    real_r_idx = @mod(real_r_idx, 10);
+    real_c_idx = @mod(real_c_idx, 10);
 
-    return state[real_r_idx][real_c_idx];
+    return state[@intCast(u32, real_r_idx)][@intCast(u32, real_c_idx)];
 }
 
-fn countNeighboursAlive(r_idx: usize, c_idx: usize) u8 {
+fn countNeighboursAlive(r_idx: i32, c_idx: i32) u8 {
     var neighbors_alive: u8 = 0;
 
     neighbors_alive += cellValue(r_idx - 1, c_idx - 1);
@@ -59,25 +61,52 @@ fn countNeighboursAlive(r_idx: usize, c_idx: usize) u8 {
 }
 
 fn nextState() void {
-    var next_state: [10][10]u8 = undefined;
-
     for (state) |row, r_idx| {
         for (row) |cell, c_idx| {
-            const neighbors_alive = countNeighboursAlive(r_idx, c_idx);
+            const neighbors_alive = countNeighboursAlive(@intCast(i32, r_idx), @intCast(i32, c_idx));
 
-            // TODO: update rules!
+            if (cell == 1 and neighbors_alive > 1 and neighbors_alive < 4) {
+                next_state[r_idx][c_idx] = 1;
+            } else if (cell == 0 and neighbors_alive == 3) {
+                next_state[r_idx][c_idx] = 1;
+            } else {
+                next_state[r_idx][c_idx] = 0;
+            }
         }
+    }
+
+    for (next_state) |row, r_idx| {
+        for (row) |cell, c_idx| {
+            state[r_idx][c_idx] = cell;
+        }
+    }
+}
+
+fn printStateMatrix(message: []const u8, state_matrix: [10][10]u8) void {
+    std.debug.print("\n\n{s}\n", .{message});
+    for (state_matrix) |row| {
+        for (row) |cell| {
+            if (cell == 0) {
+                std.debug.print(" ", .{});
+            } else {
+                std.debug.print("*", .{});
+            }
+        }
+        std.debug.print("\n", .{});
     }
 }
 
 pub fn main() !void {
     try loadState();
 
-    std.debug.print("\n\nstate matrix:\n", .{});
-    for (state) |row| {
-        for (row) |cell| {
-            std.debug.print("{}", .{cell});
-        }
-        std.debug.print("\n", .{});
+    printStateMatrix("state matrix:", state);
+
+    var x: u8 = 100;
+    while (x > 0) : (x -= 1) {
+        std.time.sleep(0.3 * std.time.ns_per_s);
+
+        nextState();
+
+        printStateMatrix("state matrix:", state);
     }
 }
