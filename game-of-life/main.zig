2dc4e84fd7af2198649deca89cb6d5f7d15b4431
Gyula Németh <gyula@chamaileon.io>
2023-08-23 12:31:23 +0200

    Day 23 - #100DaysOfCode - Refactor

--- game-of-life/main.zig
+++ game-of-life/main.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 
-const StateDescriptor = struct { n_rows: u32, n_cols: u32, memory: []u8 };
+const GameState = struct { n_rows: u32, n_cols: u32, state_matrix: []u8 };
 
-fn loadState(allocator: std.mem.Allocator) !StateDescriptor {
+fn loadState(allocator: std.mem.Allocator) !GameState {
     const state_path = "./state.txt";
 
     var state_file = try std.fs.cwd().openFile(state_path, .{});
@@ -40,68 +40,68 @@ fn loadState(allocator: std.mem.Allocator) !StateDescriptor {
         }
     }
 
-    return StateDescriptor{ .n_rows = n_rows, .n_cols = n_cols, .memory = memory };
+    return GameState{ .n_rows = n_rows, .n_cols = n_cols, .state_matrix = memory };
 }
 
 fn saveState() !void {}
 
-fn getCellValue(n_rows: u32, n_cols: u32, state_matrix: []u8, r_idx: i32, c_idx: i32) u8 {
+fn getCellValue(game_state: GameState, r_idx: i32, c_idx: i32) u8 {
     var real_r_idx = r_idx;
     var real_c_idx = c_idx;
 
     if (real_r_idx < 0) {
-        real_r_idx += @intCast(i32, n_rows);
+        real_r_idx += @intCast(i32, game_state.n_rows);
     }
 
     if (real_c_idx < 0) {
-        real_c_idx += @intCast(i32, n_cols);
+        real_c_idx += @intCast(i32, game_state.n_cols);
     }
 
-    real_r_idx = @mod(real_r_idx, @intCast(i32, n_rows));
-    real_c_idx = @mod(real_c_idx, @intCast(i32, n_cols));
+    real_r_idx = @mod(real_r_idx, @intCast(i32, game_state.n_rows));
+    real_c_idx = @mod(real_c_idx, @intCast(i32, game_state.n_cols));
 
-    return state_matrix[@intCast(u32, real_r_idx * @intCast(i32, n_cols) + real_c_idx)];
+    return game_state.state_matrix[@intCast(u32, real_r_idx * @intCast(i32, game_state.n_cols) + real_c_idx)];
 }
 
-fn setCellValue(n_cols: u32, state_matrix: []u8, r_idx: usize, c_idx: usize, value: u8) void {
-    state_matrix[r_idx * n_cols + c_idx] = value;
+fn setCellValue(game_state: GameState, r_idx: usize, c_idx: usize, value: u8) void {
+    game_state.state_matrix[r_idx * game_state.n_cols + c_idx] = value;
 }
 
-fn countNeighboursAlive(n_rows: u32, n_cols: u32, state_matrix: []u8, r_idx: i32, c_idx: i32) u8 {
+fn countNeighboursAlive(game_state: GameState, r_idx: i32, c_idx: i32) u8 {
     var neighbors_alive: u8 = 0;
 
-    neighbors_alive += getCellValue(n_rows, n_cols, state_matrix, r_idx - 1, c_idx - 1);
-    neighbors_alive += getCellValue(n_rows, n_cols, state_matrix, r_idx - 1, c_idx);
-    neighbors_alive += getCellValue(n_rows, n_cols, state_matrix, r_idx - 1, c_idx + 1);
-    neighbors_alive += getCellValue(n_rows, n_cols, state_matrix, r_idx, c_idx - 1);
-    neighbors_alive += getCellValue(n_rows, n_cols, state_matrix, r_idx, c_idx + 1);
-    neighbors_alive += getCellValue(n_rows, n_cols, state_matrix, r_idx + 1, c_idx - 1);
-    neighbors_alive += getCellValue(n_rows, n_cols, state_matrix, r_idx + 1, c_idx);
-    neighbors_alive += getCellValue(n_rows, n_cols, state_matrix, r_idx + 1, c_idx + 1);
+    neighbors_alive += getCellValue(game_state, r_idx - 1, c_idx - 1);
+    neighbors_alive += getCellValue(game_state, r_idx - 1, c_idx);
+    neighbors_alive += getCellValue(game_state, r_idx - 1, c_idx + 1);
+    neighbors_alive += getCellValue(game_state, r_idx, c_idx - 1);
+    neighbors_alive += getCellValue(game_state, r_idx, c_idx + 1);
+    neighbors_alive += getCellValue(game_state, r_idx + 1, c_idx - 1);
+    neighbors_alive += getCellValue(game_state, r_idx + 1, c_idx);
+    neighbors_alive += getCellValue(game_state, r_idx + 1, c_idx + 1);
 
     return neighbors_alive;
 }
 
-fn nextState(n_rows: u32, n_cols: u32, state_matrix: []u8, next_state_matrix: []u8) void {
-    for (state_matrix) |cell, idx| {
-        const r_idx: usize = idx / n_cols;
-        const c_idx: usize = @mod(idx, n_cols);
-        const neighbors_alive = countNeighboursAlive(n_rows, n_cols, state_matrix, @intCast(i32, r_idx), @intCast(i32, c_idx));
+fn calcNextState(current_game_state: GameState, next_game_state: GameState) void {
+    for (current_game_state.state_matrix) |cell, idx| {
+        const r_idx: usize = idx / current_game_state.n_cols;
+        const c_idx: usize = @mod(idx, current_game_state.n_cols);
+        const neighbors_alive = countNeighboursAlive(current_game_state, @intCast(i32, r_idx), @intCast(i32, c_idx));
 
         if (cell == 1 and neighbors_alive > 1 and neighbors_alive < 4) {
-            setCellValue(n_cols, next_state_matrix, r_idx, c_idx, 1);
+            setCellValue(next_game_state, r_idx, c_idx, 1);
         } else if (cell == 0 and neighbors_alive == 3) {
-            setCellValue(n_cols, next_state_matrix, r_idx, c_idx, 1);
+            setCellValue(next_game_state, r_idx, c_idx, 1);
         } else {
-            setCellValue(n_cols, next_state_matrix, r_idx, c_idx, 0);
+            setCellValue(next_game_state, r_idx, c_idx, 0);
         }
     }
 }
 
-fn printStateMatrix(message: []const u8, n_cols: u32, state_matrix: []u8) void {
+fn printStateMatrix(message: []const u8, game_state: GameState) void {
     std.debug.print("\n\n{s}", .{message});
-    for (state_matrix) |cell, idx| {
-        if (idx % n_cols == 0) {
+    for (game_state.state_matrix) |cell, idx| {
+        if (idx % game_state.n_cols == 0) {
             std.debug.print("\n", .{});
         }
         if (cell == 0) {
@@ -114,27 +114,29 @@ fn printStateMatrix(message: []const u8, n_cols: u32, state_matrix: []u8) void {
 }
 
 pub fn main() !void {
-    var stateVars = try loadState(std.heap.page_allocator);
-    defer std.heap.page_allocator.free(stateVars.memory);
+    var full_game_state: GameState = try loadState(std.heap.page_allocator);
+    defer std.heap.page_allocator.free(full_game_state.state_matrix);
 
-    var state1: []u8 = stateVars.memory[0 .. stateVars.n_rows * stateVars.n_cols];
-    var state2: []u8 = stateVars.memory[stateVars.n_rows * stateVars.n_cols ..];
+    const state_matrix_length = full_game_state.n_rows * full_game_state.n_cols;
 
-    var state: []u8 = state1;
-    var next_state: []u8 = state2;
+    var game_state_1 = GameState{ .n_rows = full_game_state.n_rows, .n_cols = full_game_state.n_cols, .state_matrix = full_game_state.state_matrix[0..state_matrix_length] };
+    var game_state_2 = GameState{ .n_rows = full_game_state.n_rows, .n_cols = full_game_state.n_cols, .state_matrix = full_game_state.state_matrix[state_matrix_length..] };
 
-    printStateMatrix("state: matrix:", stateVars.n_cols, state);
+    var state: GameState = game_state_1;
+    var next_state: GameState = game_state_2;
+
+    printStateMatrix("state: matrix:", state);
 
     var x: u8 = 100;
     while (x > 0) : (x -= 1) {
         std.time.sleep(0.3 * std.time.ns_per_s);
 
-        nextState(stateVars.n_rows, stateVars.n_cols, state, next_state);
+        calcNextState(state, next_state);
 
         var tmp = state;
         state = next_state;
         next_state = tmp;
 
-        printStateMatrix("state matrix:", stateVars.n_cols, state);
+        printStateMatrix("state matrix:", state);
     }
 }
