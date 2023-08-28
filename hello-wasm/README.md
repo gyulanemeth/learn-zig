dc0b5c511f15a510d144e42cace53ce0fd387709
Gyula Németh <gyula@chamaileon.io>
2023-08-28 08:11:39 +0200

    Day 28 - #100DaysOfCode - Finally! WASM works!

---
+++ hello-wasm/README.md
@@ -0,0 +1,4 @@
+Needed to compile with the following args:
+```
+zig build-lib main.zig -target wasm32-freestanding -dynamic -rdynamic
+```
--- hello-wasm/main.zig
+++ hello-wasm/main.zig
@@ -1,5 +1,9 @@
 extern fn inc(a: i32) i32;
 
+export fn yo(a: i32) i32 {
+    return a + 2;
+}
+
 export fn addInc(a: i32, b: i32) i32 {
     return inc(a) + b;
 }
