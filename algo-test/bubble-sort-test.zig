5000e9d7c0762a908fd6f2cd1053a832e0b98241
Gyula Németh <gyula@chamaileon.io>
2023-09-22 18:04:54 +0200

    Day 53 - #100DaysOfCode - algo test in progress

--- algo-test/bubble-sort-test.zig
+++ algo-test/bubble-sort-test.zig
@@ -1,12 +1,13 @@
 const std = @import("std");
 
-// extern fn swapIfNeeded(array, idx1, idx2) void;
-
+extern fn swapIfNeeded(array: [*]i32, idx1: usize, idx2: usize) callconv(.C) void;
+extern fn logRef(arr: [*]i32) callconv(.C) void;
+extern fn logSol(arr: [*]i32) callconv(.C) void;
 
 var arraySol = [_]i32{ 5, 2, 1, 3, 4 };
 var arrayRef = [_]i32{ 5, 2, 1, 3, 4 };
 
-fn check() bool {
+fn areTheSame() bool {
     for (arrayRef, 0..) |act, idx| {
         if (act != arraySol[idx]) {
             return false;
@@ -15,36 +16,40 @@ fn check() bool {
     return true;
 }
 
-fn swapIfNeededRef(idx1: usize, idx2: usize) void {
+fn swapIfNeededRef(idx1: usize, idx2: usize) bool {
     if (arrayRef[idx1] > arrayRef[idx2]) {
         const temp = arrayRef[idx1];
         arrayRef[idx1] = arrayRef[idx2];
         arrayRef[idx2] = temp;
+
+        return true;
     }
+    return false;
 }
 
-fn printArrayRef() void {
-    for(arrayRef) |item| {
-        std.debug.print("{d} ", .{ item });
-    }
-    std.debug.print("\n", .{});
+export fn logArrays() void {
+    logRef(&arrayRef);
+    logSol(&arraySol);
 }
 
-fn sort() void {
+export fn sort() bool {
     var idx_outer: usize = arrayRef.len;
     while(idx_outer > 0) : (idx_outer -= 1) {
         var idx_inner: usize = 0;
+        var swapped: bool = false;
         while (idx_inner < idx_outer - 1) : (idx_inner += 1) {
-            swapIfNeededRef(idx_inner, idx_inner + 1);
-            printArrayRef();
+            swapped = swapIfNeededRef(idx_inner, idx_inner + 1) or swapped;
+            swapIfNeeded(&arraySol, idx_inner, idx_inner + 1);
+
+            logArrays();
+
+            if (!areTheSame()) {
+                return false;
+            }
+        }
+        if (!swapped) {
+            break;
         }
     }
-}
-
-pub fn main() void {
-    std.debug.print("Original\n", .{});
-    printArrayRef();
-    std.debug.print("Start\n", .{});
-    sort();
-    std.debug.print("End\n", .{});
+    return true;
 }
