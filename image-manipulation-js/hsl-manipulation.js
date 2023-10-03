e1c3f1b57520777f5bc232583b0ccad790248918
Gyula Németh <gyula@chamaileon.io>
2023-10-03 08:27:11 +0200

    Day 63 #100DaysOfCode everything->selection-based

--- image-manipulation-js/hsl-manipulation.js
+++ image-manipulation-js/hsl-manipulation.js
@@ -1,138 +1,60 @@
-export default () => {
-    function rotateHueBy10Deg() {
-        const start = performance.now()
+export function addHslaToSelection(imgData, selection, hslaDiff) {
+    for (let idx = 0; idx < imgData.data.length; idx += 4) {
+        const coordIdx = idx / 4
 
-        const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+        const r = imgData.data[idx]
+        const g = imgData.data[idx + 1]
+        const b = imgData.data[idx + 2]
+        const a = imgData.data[idx + 3]
 
-        for (let idx = 0; idx < imgData.data.length; idx += 4) {
-            if (selection[idx / 4] === 0) {
-                continue
-            }
-            const r = imgData.data[idx]
-            const g = imgData.data[idx + 1]
-            const b = imgData.data[idx + 2]
-            const a = imgData.data[idx + 3]
-
-            const hsla = rgbToHsl({ r, g, b, a })
-            hsla.h += 10
-            hsla.h %= 360
-
-            const newRgba = hslToRgb(hsla)
-            imgData.data[idx] = newRgba.r
-            imgData.data[idx + 1] = newRgba.g
-            imgData.data[idx + 2] = newRgba.b
-            imgData.data[idx + 3] = newRgba.a
+        if (selection[coordIdx] != 1) {
+            continue
         }
 
-        context.putImageData(imgData, 0, 0)
-        const execSpan = document.getElementById('exec-time')
-        execSpan.innerHTML = performance.now() - start
-    }
+        const hsla = rgbToHsl({ r, g, b, a })
+        hsla.h += hslaDiff.h
+        hsla.s += hslaDiff.s
+        hsla.l += hslaDiff.l
+        hsla.a += hslaDiff.a
 
-    
-
-    function addSaturationToSelection(saturationDiff) {
-        const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
-        for (let idx = 0; idx < imgData.data.length; idx += 4) {
-            const coordIdx = idx / 4
-
-            const r = imgData.data[idx]
-            const g = imgData.data[idx + 1]
-            const b = imgData.data[idx + 2]
-            const a = imgData.data[idx + 3]
-
-            if (selection[coordIdx] != 1) {
-                continue
-            }
-
-            const hsla = rgbToHsl({ r, g, b, a })
-            hsla.s += saturationDiff
-
-            if (hsla.s > 1) {
-                hsla.s = 1
-            }
-
-            if (hsla.s < 0) {
-                hsla.s = 0
-            }
-
-            const newRgba = hslToRgb(hsla)
-            imgData.data[idx] = newRgba.r
-            imgData.data[idx + 1] = newRgba.g
-            imgData.data[idx + 2] = newRgba.b
-            imgData.data[idx + 3] = newRgba.a
+        if (hsla.h < 0) {
+            hsla.h += 360
         }
-        context.putImageData(imgData, 0, 0)
-        drawSelectedPixels()
-    }
 
-    function addLightnessToSelection(lightnessDiff) {
-        const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
-        for (let idx = 0; idx < imgData.data.length; idx += 4) {
-            const coordIdx = idx / 4
-
-            const r = imgData.data[idx]
-            const g = imgData.data[idx + 1]
-            const b = imgData.data[idx + 2]
-            const a = imgData.data[idx + 3]
-
-            if (selection[coordIdx] != 1) {
-                continue
-            }
-
-            const hsla = rgbToHsl({ r, g, b, a })
-            hsla.l += lightnessDiff
-
-            if (hsla.l > 1) {
-                hsla.l = 1
-            }
-
-            if (hsla.l < 0) {
-                hsla.l = 0
-            }
-
-            const newRgba = hslToRgb(hsla)
-            imgData.data[idx] = newRgba.r
-            imgData.data[idx + 1] = newRgba.g
-            imgData.data[idx + 2] = newRgba.b
-            imgData.data[idx + 3] = newRgba.a
+        if (hsla.h >= 360) {
+            hsla.h -= 360
         }
-        context.putImageData(imgData, 0, 0)
-        drawSelectedPixels()
-    }
 
-    function addHueToSelection(hueDiff) {
-        const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
-        for (let idx = 0; idx < imgData.data.length; idx += 4) {
-            const coordIdx = idx / 4
-
-            const r = imgData.data[idx]
-            const g = imgData.data[idx + 1]
-            const b = imgData.data[idx + 2]
-            const a = imgData.data[idx + 3]
-
-            if (selection[coordIdx] != 1) {
-                continue
-            }
-
-            const hsla = rgbToHsl({ r, g, b, a })
-            hsla.h += hueDiff
-
-            if (hsla.h < 0) {
-                hsla.h += 360
-            }
-
-            if (hsla.h >= 360) {
-                hsla.h -= 360
-            }
-
-            const newRgba = hslToRgb(hsla)
-            imgData.data[idx] = newRgba.r
-            imgData.data[idx + 1] = newRgba.g
-            imgData.data[idx + 2] = newRgba.b
-            imgData.data[idx + 3] = newRgba.a
+        if (hsla.l > 1) {
+            hsla.l = 1
         }
-        context.putImageData(imgData, 0, 0)
-        drawSelectedPixels()
+
+        if (hsla.l < 0) {
+            hsla.l = 0
+        }
+
+        if (hsla.s > 1) {
+            hsla.s = 1
+        }
+
+        if (hsla.s < 0) {
+            hsla.s = 0
+        }
+
+        if (hsla.a < 0) {
+            hsla.a = 0
+        }
+
+        if (hsla.a > 255) {
+            hsla.a = 255
+        }
+
+        const newRgba = hslToRgb(hsla)
+        imgData.data[idx] = newRgba.r
+        imgData.data[idx + 1] = newRgba.g
+        imgData.data[idx + 2] = newRgba.b
+        imgData.data[idx + 3] = newRgba.a
     }
+
+    return imgData
 }
