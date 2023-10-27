275dc68354d873572ad86f13c1bb0dc51a796d4e
Gyula Németh <gyula@chamaileon.io>
2023-10-27 16:19:52 +0200

    Day 87 - #100DaysOfCode - thinking about selection

---
+++ image-manipulation-zig/hsl-manipulation.zig
@@ -0,0 +1,1 @@
+pub fn addHslaToSelection() void {}
--- image-manipulation-zig/image-manipulation.zig
+++ image-manipulation-zig/image-manipulation.zig
@@ -16,17 +16,25 @@ var state_2: ImageData = undefined;
 var current_state: ImageData = undefined;
 var next_state: ImageData = undefined;
 
-export fn init(n_rows: u32, n_cols: u32) callconv(.C) [*]u8 {
-    const num_color_values = n_rows * n_cols * 4; // 4x -> r, g, b, a;
+const SelectionData = struct { width: u32, height: u32, selection: []u8 };
+
+const selection: SelectionData = undefined;
+
+export fn init(height: u32, width: u32) callconv(.C) [*]u8 {
+    const num_color_values = height * width * 4; // 4x -> r, g, b, a;
     const memory_size = num_color_values * 2; // 2x -> current_state, next_state
     var memory = std.heap.wasm_allocator.alloc(u8, memory_size) catch unreachable;
 
-    full_state = ImageData{ .n_rows = n_rows, .n_cols = n_cols, .data = memory };
-    state_1 = ImageData{ .n_rows = full_state.n_rows, .n_cols = full_state.n_cols, .data = full_state.data[0..num_color_values] };
-    state_2 = ImageData{ .n_rows = full_state.n_rows, .n_cols = full_state.n_cols, .data = full_state.data[num_color_values..] };
+    var selection = std.heap.wasm_allocator.alloc(u8, height * width);
+
+    full_state = ImageData{ .height = height, .width = width, .data = memory };
+    state_1 = ImageData{ .height = full_state.height, .width = full_state.width, .data = full_state.data[0..num_color_values] };
+    state_2 = ImageData{ .height = full_state.height, .width = full_state.width, .data = full_state.data[num_color_values..] };
 
     current_state = state_1;
     next_state = state_2;
+
+    selection = SelectionData{ .width = full_state.width, .height = full_state.height, .selection = selection[0 .. width * height] };
 
     return memory.ptr;
 }
