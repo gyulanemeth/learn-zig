862d2ea480be80c4a9bd548dd9f11a33e6e41a18
Gyula Németh <gyula@chamaileon.io>
2023-10-30 08:01:11 +0100

    Day 90 - #100DaysOfCode - selection based on hue

--- image-manipulation-zig/selection.zig
+++ image-manipulation-zig/selection.zig
@@ -1,4 +1,12 @@
 const std = @import("std");
+const ArrayList = std.ArrayList;
+
+const convert = @import("./convert.zig");
+
+const RgbPixel = convert.RgbPixel;
+const HslPixel = convert.HslPixel;
+const rgb_to_hsl = convert.rgb_to_hsl;
+const hsl_to_rgb = convert.hsl_to_rgb;
 
 const SelectionData = struct { width: u32, height: u32, data: []u8 };
 
@@ -49,6 +57,82 @@ pub fn rectangularSelection(from: Coord, to: Coord) void {
     }
 }
 
-pub fn addToSelectionBasedOnHslRange() void {}
+pub fn setSelectionBasedOnHslRange(img: ImageData, coord: Coord, range: HslPixel, value: u8) void {
+    const rgb_start_px = img.get_pixel(coord);
+    const hsl_start_px = rgb_to_hsl(rgb_start_px);
+
+    const h_min = hsl_start_px.hue - range.hue;
+    const h_max = hsl_start_px.hue + range.hue;
+    const l_min = hsl_start_px.l - range.l;
+    const l_max = hsl_start_px.l + range.l;
+    const s_min = hsl_start_px.s - range.s;
+    const s_max = hsl_start_px.s + range.s;
+
+    const visited = std.heap.wasm_allocator.alloc(u8, img.width * img.height);
+    defer std.heap.wasm_allocator.free(visited);
+
+    const coors_to_visit = ArrayList(Coord).init(std.heap.wasm_allocator);
+    defer coors_to_visit.deinit();
+
+    coors_to_visit.append(coord);
+
+    while (coors_to_visit.items.len > 0) {
+        const act_coord = coors_to_visit.pop();
+
+        if (act_coord.x < 0 || act_coord.y < 0) {
+          continue;
+        }
+
+        if (act_coord.x >= img.width || act_coord.y >= img.height) {
+          continue;
+        }
+
+        if (visited[act_coord.y * img.width + act_coord.x] == 1) {
+          continue;
+        }
+
+        visited[act_coord.y * img.width + act_coord.x] = 1;
+
+        const act_rgb = img.get_pixel(act_coord);
+        const act_hsl = rgb_to_hsl(act_rgb);
+
+        if (act_hsl.h < h_min) {
+          continue;
+        }
+
+        if (act_hsl.h > h_max) {
+          continue;
+        }
+
+        if (act_hsl.l < l_min) {
+          continue;
+        }
+
+        if (act_hsl.l > l_max) {
+          continue;
+        }
+
+        if (act_hsl.s < s_min) {
+          continue;
+        }
+
+        if (act_hsl.s > s_max) {
+          continue;
+        }
+
+        selection[act_coord.y * img.width + act_coord.x] = 1;
+
+        coords_to_visit.append(Coord{ .y = act_coord.y - 1, .x = act_coord.x - 1 })
+        coords_to_visit.append(Coord{ .y = act_coord.y - 1, .x = act_coord.x })
+        coords_to_visit.append(Coord{ .y = act_coord.y - 1, .x = act_coord.x + 1 })
+
+        coords_to_visit.append(Coord{ .y = act_coord.y, .x = act_coord.x - 1 })
+        coords_to_visit.append(Coord{ .y = act_coord.y, .x = act_coord.x + 1 })
+
+        coords_to_visit.append(Coord{ .y = act_coord.y + 1, .x = act_coord.x - 1 })
+        coords_to_visit.append(Coord{ .y = act_coord.y + 1, .x = act_coord.x })
+        coords_to_visit.append(Coord{ .y = act_coord.y + 1, .x = act_coord.x + 1 })
+    }
+}
 
 pub fn dilate() void {}
